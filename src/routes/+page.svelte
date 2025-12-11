ebd187bc29183a03f972cfe48aa7450b65a05b45
Al Wassi Khan <145786993+wassi-real@users.noreply.github.com>
2025-12-11 20:59:26 +0600

    Delete src directory

--- src/routes/+page.svelte
+++
@@ -1,5 +0,0 @@
-<script lang="ts">
-	import Hero from '$lib/components/Hero.svelte';
-</script>
-
-<Hero />
--- src/routes/dashboard/agencies/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import type { PageServerLoad } from './$types';
-import { redirect } from '@sveltejs/kit';
-
-export const load: PageServerLoad = async ({ url }) => {
-	// Redirect to stack page with agencies tab
-	throw redirect(302, '/dashboard/stack');
-};
-
